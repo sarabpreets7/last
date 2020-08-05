0702a5711d6bbb43718a66fb9237339ae18e74d0
sarabpreets7 <56184253+sarabpreets7@users.noreply.github.com>
2020-08-05 08:56:47 +0530

    Update wsgi.py

--- last/wsgi.py
+++ last/wsgi.py
@@ -6,7 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-
+python_home='/path/to/your/venv'
+activate_this=python_home+'/bin/activate_this.py'
+with open(activate_this) as file_:
+        exec(file_.read(), dict(__file__=activate_this))
 import os
 
 from django.core.wsgi import get_wsgi_application
